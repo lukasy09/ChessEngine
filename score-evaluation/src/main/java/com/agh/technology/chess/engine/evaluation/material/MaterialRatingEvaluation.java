98725c3cda3bd95c085f384f8f3f1c8910ea716e
lukasy09 <lukasy09@gmail.com>
2020-08-17 20:20:09 +0200

    Material value evaluation

--- score-evaluation/src/main/java/com/agh/technology/chess/engine/evaluation/material/MaterialRatingEvaluation.java
+++ score-evaluation/src/main/java/com/agh/technology/chess/engine/evaluation/material/MaterialRatingEvaluation.java
@@ -1,42 +1,80 @@
 package com.agh.technology.chess.engine.evaluation.material;
 
 import com.agh.technology.chess.engine.model.element.PieceType;
+import com.agh.technology.chess.engine.model.state.BoardState;
+
+import static com.agh.technology.chess.engine.evaluation.material.MaterialValueCalculator.calculatePieceMaterialValue;
 
 public class MaterialRatingEvaluation {
 
 
-    private int computeMaterialValue(PieceType piece) {
-        int materialValue = 0;
-        switch (piece) {
-            case PAWN:
-                materialValue = 100;
-                break;
+    public int evaluateTotalRating(BoardState boardState) {
+        int positionScore = 0;
+        positionScore += evaluateMaterialRating(boardState);
 
-            case KNIGHT:
-                materialValue = 320;
-                break;
-
-            case BISHOP:
-                materialValue = 333;
-                break;
-
-            case ROOK:
-                materialValue = 510;
-                break;
-
-            case QUEEN:
-                materialValue = 880;
-                break;
-
-            case KING:
-                materialValue = 5000;
-                break;
-        }
-        return materialValue;
+        return positionScore;
     }
 
 
+    private int evaluateMaterialRating(BoardState boardState) {
+        int materialScoreAccumulator = 0;
+
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getWhiteState().getKing(), PieceType.KING);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getWhiteState().getQueen(), PieceType.QUEEN);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getWhiteState().getRook(), PieceType.ROOK);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getWhiteState().getBishop(), PieceType.BISHOP);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getWhiteState().getKnight(), PieceType.KNIGHT);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getWhiteState().getPawn(), PieceType.PAWN);
+
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getBlackState().getQueen(), PieceType.QUEEN);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getBlackState().getQueen(), PieceType.QUEEN);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getBlackState().getRook(), PieceType.ROOK);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getBlackState().getBishop(), PieceType.BISHOP);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getBlackState().getKnight(), PieceType.KNIGHT);
+        materialScoreAccumulator += accumulateMaterialRatingForPieceType(boardState.getBlackState().getPawn(), PieceType.PAWN);
+
+        return materialScoreAccumulator;
+    }
+
+    private int accumulateMaterialRatingForPieceType(long pieceTypePosition, PieceType pieceType) {
+        int partScore = 0;
+        while (pieceTypePosition != 0) {
+            pieceTypePosition = pieceTypePosition & (pieceTypePosition - 1);
+            partScore += calculateMaterialValue(pieceType);
+        }
+        return partScore;
+    }
 
 
+    private int calculateMaterialValue(PieceType piece) {
+        try {
+            switch (piece) {
+                case PAWN:
+                    return (int) calculatePieceMaterialValue(PieceType.PAWN);
 
+                case KNIGHT:
+                    return (int) calculatePieceMaterialValue(PieceType.KNIGHT);
+
+                case BISHOP:
+                    return (int) calculatePieceMaterialValue(PieceType.BISHOP);
+
+                case ROOK:
+                    return (int) calculatePieceMaterialValue(PieceType.ROOK);
+
+                case QUEEN:
+                    return (int) calculatePieceMaterialValue(PieceType.QUEEN);
+
+                case KING:
+                    return (int) calculatePieceMaterialValue(PieceType.KING);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+//       MaterialRatingEvaluation evaluation = new MaterialRatingEvaluation();
+//        System.out.println(evaluation.calculateMaterialValue(PieceType.QUEEN));
+    }
 }
--- score-evaluation/src/main/java/com/agh/technology/chess/engine/evaluation/material/MaterialValueCalculator.java
+++ score-evaluation/src/main/java/com/agh/technology/chess/engine/evaluation/material/MaterialValueCalculator.java
@@ -3,7 +3,7 @@ package com.agh.technology.chess.engine.evaluation.material;
 import com.agh.technology.chess.engine.model.element.PieceType;
 
 @SuppressWarnings("FieldCanBeLocal")
-public class MateriaValueScaler {
+class MaterialValueCalculator {
 
     private static int KING_TRADITIONAL_MATERIAL_VALUE = 5000;
     private static int QUEEN_TRADITIONAL_MATERIAL_VALUE = 9;
@@ -13,19 +13,18 @@ public class MateriaValueScaler {
     private static int PAWN_TRADITIONAL_MATERIAL_VALUE = 1;
 
     private static double KING_LOGIC_FACTOR = 1d;
-    private static double QUEEN_LOGIC_FACTOR = 8.9/9;
+    private static double QUEEN_LOGIC_FACTOR = 8.8 / 9;
     private static double ROOK_LOGIC_FACTOR = 1.02;
-    private static double KNIGHT_LOGIC_FACTOR = 1.01;
-    private static double BISHOP_LOGIC_FACTOR = 1.06667;
+    private static double KNIGHT_LOGIC_FACTOR = 1.06667;
+    private static double BISHOP_LOGIC_FACTOR = 1.1;
     private static double PAWN_LOGIC_FACTOR = 1.06667;
 
     private static int SCALE_FACTOR = 100;
 
 
-    public static double scaleMaterialByPieceType(PieceType pieceType){
+    static double calculatePieceMaterialValue(PieceType pieceType) {
 
-        switch (pieceType){
-
+        switch (pieceType) {
             case KING:
                 return KING_TRADITIONAL_MATERIAL_VALUE * KING_LOGIC_FACTOR * SCALE_FACTOR;
             case QUEEN:
@@ -38,9 +37,9 @@ public class MateriaValueScaler {
                 return KNIGHT_TRADITIONAL_MATERIAL_VALUE * KNIGHT_LOGIC_FACTOR * SCALE_FACTOR;
             case PAWN:
                 return PAWN_TRADITIONAL_MATERIAL_VALUE * PAWN_LOGIC_FACTOR * SCALE_FACTOR;
-                default:
-                System.out.println("Wrong piece!");
-                return -1;
+            default:
+                System.out.println("Wrong piece type!");
+                return 0;
         }
     }
 }
