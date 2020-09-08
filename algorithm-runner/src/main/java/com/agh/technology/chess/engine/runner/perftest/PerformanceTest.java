a0dc79ba9cdd7e3cc27639cafd7e97104819c19d
Alicja Smolikowska <a.smolikowska@gmail.com>
2020-09-08 16:18:07 +0200

    fix performance tests

--- algorithm-runner/src/main/java/com/agh/technology/chess/engine/runner/perftest/PerformanceTest.java
+++ algorithm-runner/src/main/java/com/agh/technology/chess/engine/runner/perftest/PerformanceTest.java
@@ -7,19 +7,24 @@ import com.agh.technology.chess.engine.runner.Algorithm;
 import static java.lang.Long.parseLong;
 
 public class PerformanceTest {
-    private static final Algorithm algorithm = new Algorithm();
+
 
     private static void simpleBoardTest() {
         System.out.println("Initial board performance test");
-        BoardState boardState = new BoardState();
-        long startTime = System.nanoTime();
-        algorithm.minmax(boardState, 6, Color.WHITE);
-        long endTime =  System.nanoTime();
-        System.out.println("That took " + (endTime - startTime) + " nanoseconds");
+        BoardState boardState = BoardState.getStartingState();
+        Algorithm algorithm = new Algorithm();
+        for (int i = 1; i < 9; i++) {
+            long startTime = System.nanoTime();
+            algorithm.minmax(boardState, i, Color.WHITE);
+            long endTime = System.nanoTime();
+            System.out.println("That took " + (endTime - startTime) + " nanoseconds");
+        }
     }
-    private static void harderBoardTest() {
+
+    private static void minMaxBothKingPerfTest() {
         System.out.println("Board performance test");
-        BoardState boardState = new BoardState();
+        BoardState boardState = BoardState.getStartingState();
+        Algorithm algorithm = new Algorithm();
         long bishop = parseLong("0010010000000000000000000000000000000000000000000000000000000000", 2);
         long king = parseLong("0000000010000000000000000000000000000000000000000000000000000000", 2);
         long knight = parseLong("0000000000100000000000000000000000000000000000000000000000000000", 2);
@@ -27,7 +32,7 @@ public class PerformanceTest {
         long queen = parseLong("0000000000000000000000000010000000000000000000000000000000000000", 2);
         long rook = parseLong("0000000000000000000000000010000000000000000000000000000000000000", 2);
 
-        long whiteKing = parseLong("0000000000000000000000000000000000000000000000000000000000000000", 2);
+        long whiteKing = parseLong("0000000000010000000000000000000000000000000000000000000000000000", 2);
         long whiteKnight = parseLong("0000000000000001000000000000000000000000000000000000000000000000", 2);
         long whiteBishop = parseLong("0000000000000000000000000000000000000000000000000000000000000000", 2);
         long whitePawn = parseLong("0000000000000000000000000000000000000000000000000000000011111111", 2);
@@ -47,14 +52,16 @@ public class PerformanceTest {
         boardState.getWhiteState().setPawn(whitePawn);
         boardState.getWhiteState().setQueen(whiteQueen);
         boardState.getWhiteState().setRook(whiteRook);
-        long startTime = System.nanoTime();
-        algorithm.minmax(boardState, 6, Color.WHITE);
-        long endTime =  System.nanoTime();
-        System.out.println("That took " + (endTime - startTime) + " nanoseconds");
+        for (int i = 1; i < 9; i++) {
+            long startTime = System.nanoTime();
+            algorithm.minmax(boardState, i, Color.WHITE);
+            long endTime = System.nanoTime();
+            System.out.println("That took " + (endTime - startTime) + " nanoseconds");
+        }
     }
 
     public static void main(String[] args) {
         simpleBoardTest();
-        harderBoardTest();
+        minMaxBothKingPerfTest();
     }
 }
